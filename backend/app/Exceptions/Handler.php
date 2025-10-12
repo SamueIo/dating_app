786d2f72798554dabfe2ddd554635f4e4a1f04f9
SamueIo <samueljanech7@gmail.com>
2025-10-12 16:28:00 +0200

    Logging.php for displaying logs

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -6,14 +6,27 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
 use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class Handler extends ExceptionHandler
 {
-    // ... ostatné metódy ...
+    // Ak tam je vlastné $dontReport, nechaj tak, inak môžeš pridať podľa potreby
+
+    public function report(Throwable $exception)
+    {
+        // Tu logujeme každú výnimku (error)
+        Log::error('Exception caught: ' . $exception->getMessage(), [
+            'exception' => $exception,
+            'trace' => $exception->getTraceAsString(),
+        ]);
+
+        parent::report($exception);
+    }
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        \Log::info('unauthenticated called', ['url' => $request->url()]);
+        Log::info('unauthenticated called', ['url' => $request->url()]);
+
         if ($request->expectsJson() || $request->is('broadcasting/auth')) {
             return response()->json(['message' => 'Unauthenticated.'], 401);
         }
