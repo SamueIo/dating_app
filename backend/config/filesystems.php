642a9a64f661cf4899a8f5ad3fda96ecea60f7a2
SamueIo <samueljanech7@gmail.com>
2025-10-13 20:16:35 +0200

    Creating alternative for storage:link and updating env and filesystems

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -38,15 +38,21 @@ return [
             'report' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => env('APP_STORAGE_PATH', storage_path('app/public')),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -59,6 +65,7 @@ return [
             'throw' => false,
             'report' => false,
         ],
+
 
     ],
 
