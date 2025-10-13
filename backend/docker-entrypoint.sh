91f7aebc017deaac496735663bf668dbf622157b
SamueIo <samueljanech7@gmail.com>
2025-10-13 20:30:17 +0200

    Docker file updated

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-# Link to storage
-php artisan storage:link
+# Remove old symlink if it exists
+rm -rf public/storage
 
-# Run apache
+# Create a new symlink to the volume mount point
+ln -s /app/storage/app/public public/storage
+
+# Set permissions (recommended)
+chown -R www-data:www-data /app/storage
+chmod -R 775 /app/storage
+
+# Start Apache in the foreground
 exec apache2-foreground
