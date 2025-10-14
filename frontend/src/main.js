6da431e8c77027b07565d0bdb40bba75f11bb215
SamueIo <samueljanech7@gmail.com>
2025-10-14 21:42:10 +0200

    Echo.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,7 @@ import {createPinia} from 'pinia'
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000';
 console.log('axios.defaults.baseURL',axios.defaults.baseURL);
 
 
