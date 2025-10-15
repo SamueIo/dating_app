a602fd57b09ef5aca2f60db44b10cc7e40e86bed
SamueIo <samueljanech7@gmail.com>
2025-10-15 21:31:14 +0200

    Docker-entrypoint.sh fixing symlink

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "[entrypoint] Čakám na storage volume..."
-until [ -d /var/www/html/storage/app/public ]; do
+until [ -d /app/storage/app/public ]; do
     sleep 1
 done
 
-echo "[entrypoint] Vytváram symlink public/storage..."
 php artisan storage:link
 
-echo "[entrypoint] Nastavujem práva na storage..."
-chown -R www-data:www-data /var/www/html/storage
-chmod -R 775 /var/www/html/storage
-find /var/www/html/storage -type f -exec chmod 664 {} \;
-find /var/www/html/storage -type d -exec chmod 775 {} \;
+chown -R www-data:www-data /app/storage
+chmod -R 775 /app/storage
+find /app/storage -type f -exec chmod 664 {} \;
+find /app/storage -type d -exec chmod 775 {} \;
+
 
 echo "[entrypoint] Spúšťam Laravel server..."
 exec "$@"
