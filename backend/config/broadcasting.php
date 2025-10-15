860018b78fa8d2f16aa3cf2adfa9582034d5398b
SamueIo <samueljanech7@gmail.com>
2025-10-15 22:34:09 +0200

    Fixing broadcasting config

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -30,17 +30,18 @@ return [
 
     'connections' => [
 
-       'reverb' => [
+        'reverb' => [
            'driver' => 'reverb',
            'key' => env('PUSHER_APP_KEY'),
            'secret' => env('PUSHER_APP_SECRET'),
            'app_id' => env('PUSHER_APP_ID'),
            'options' => [
-               'host' => '127.0.0.1',
-               'port' => 8080,
-               'scheme' => 'http',
+               'host' => env('REVERB_HOST', '127.0.0.1'),
+               'port' => env('REVERB_PORT', 6001),
+               'scheme' => env('REVERB_SCHEME', 'http'),
            ],
-       ],
+        ],
+
     ],
 
 ];
