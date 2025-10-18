3363ae509c933981841640c7cc45bc9ad444c47f
SamueIo <samueljanech7@gmail.com>
2025-10-18 15:24:13 +0200

    Echo

--- backend/resources/js/echo.js
+++ backend/resources/js/echo.js
@@ -15,25 +15,16 @@
 
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
-const host = import.meta.env.VITE_REVERB_HOST === '0.0.0.0'
-  ? window.location.hostname
-  : import.meta.env.VITE_REVERB_HOST;
-
-const port = import.meta.env.VITE_REVERB_PORT ?? (window.location.protocol === 'https:' ? 443 : 80);
-const forceTLS = (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https';
-
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-    authEndpoint: 'https://datingapp-production-fa2b.up.railway.app/broadcasting/auth',
-    auth: {
-        withCredentials: true,
-    },
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT,
+  wssPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
 });
+
