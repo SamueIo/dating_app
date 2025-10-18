d5dcfc3d5412e006637c0bc3fbdfdcbf09a25a6b
SamueIo <samueljanech7@gmail.com>
2025-10-18 14:41:16 +0200

    Echo

--- backend/resources/js/echo.js
+++ backend/resources/js/echo.js
@@ -25,12 +25,15 @@ const port = import.meta.env.VITE_REVERB_PORT ?? (window.location.protocol === '
 const forceTLS = (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https';
 
 window.Echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: host,
-  wsPort: forceTLS ? null : port,   // Ak je TLS, port sa nešpecifikuje (WSS ide defaultne na 443)
-  wssPort: forceTLS ? port : null,  // Ak je TLS, wssPort je port, inak null
-  forceTLS: forceTLS,
-  enabledTransports: ['ws', 'wss'],
-  disableStats: true,
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+    authEndpoint: 'https://datingapp-production-fa2b.up.railway.app/broadcasting/auth',
+    auth: {
+        withCredentials: true,
+    },
 });
