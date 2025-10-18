5a5cda5abf24002900d925b9c4ba4347f17af564
SamueIo <samueljanech7@gmail.com>
2025-10-18 13:18:24 +0200

    Procfile

--- frontend/src/components/layouts/GuestLayout.vue
+++ frontend/src/components/layouts/GuestLayout.vue
@@ -1,17 +1,14 @@
 <template>
-  <div
-    class="flex items-center justify-center w-screen h-screen p-1  md:p-16"  
-  >
+  <div class="flex items-center justify-center w-screen h-screen overflow-auto">
+
     <div
       class="flex flex-col items-center justify-center p-1 sm:p-4 md:p-10 lg:p-14
         rounded-2xl border border-pink-400/30 bg-white/10 backdrop-blur-md shadow-2xl
         ring-1 ring-white/20 transition-all duration-500 hover:shadow-pink-500/30
         w-full max-w-md md:max-w-lg"  
     >
-    <RouterView />
-<slot>
-  
-</slot>
+      <RouterView />
+      <slot></slot>
     </div>
   </div>
 </template>
