0b7aafd5be2bc97a2591c00e28456b68e5cee650
SamueIo <samueljanech7@gmail.com>
2025-10-26 11:39:13 +0100

    Entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[entrypoint] Čakám na mount volume..."
-while [ ! -d /app/storage/app/public ]; do
-  echo "Volume ešte nie je pripojené, čakám..."
-  sleep 1
-done
+php artisan storage:link
 
-echo "[entrypoint] Volume pripojené ✅"
-
-if [ ! -L /app/public/storage ]; then
-    echo "[entrypoint] Vytváram storage link..."
-    php artisan storage:link --relative || true
-else
-    echo "[entrypoint] Storage link už existuje."
-fi
 
 chown -R www-data:www-data /app/storage
 chmod -R 775 /app/storage
